7876d72529ed3ecda1f8f6f3be2d537f9b7672d1
Andrew <andrew.vinocur@gmail.com>
2018-06-18 14:24:11 -0700

    adds autoComplete prop

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -8,11 +8,21 @@ class Signup extends Component {
       <form>
         <fieldset>
           <label>Email</label>
-
+          <Field
+            name="email"
+            type="text"
+            component="input"
+            autoComplete="none"
+          />
         </fieldset>
         <fieldset>
           <label>Password</label>
-
+          <Field
+            name="password"
+            type="password"
+            component="input"
+            autoComplete="none"
+          />
         </fieldset>
       </form>
     );
