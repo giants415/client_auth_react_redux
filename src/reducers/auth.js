70efa0bc11f6a33bd8641b1df45e9aa81adeb9bb
Andrew <andrew.vinocur@gmail.com>
2018-06-25 12:51:01 -0700

    refactors to use AUTH_USER

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -6,5 +6,10 @@ const INITIAL_STATE = {
 };
 
 export default function(state = INITIAL_STATE, action) {
-  return state;
+  switch(action.type) {
+    case AUTH_USER:
+      return { ...state, authenticated: action.payload };
+    default:
+      return state;
+  }
 }
