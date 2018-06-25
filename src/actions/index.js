a6ca056d5826ca213b2a7bd8ab8e9d5412904c00
Andrew <andrew.vinocur@gmail.com>
2018-06-25 12:47:25 -0700

    finishes action for now

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,5 +4,5 @@ import { AUTH_USER } from './types';
 export const signup = formProps => async dispatch => {
   const response = await axios.post('http://localhost:3090/signup', formProps);
 
-  
+  dispatch({ type: AUTH_USER, payload: response.data.token });
 };
