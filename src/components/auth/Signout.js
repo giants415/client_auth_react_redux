02313904d9f921d9b70b9b3eef4274fa430002a8
Andrew <andrew.vinocur@gmail.com>
2018-06-25 15:20:39 -0700

    imports and connect

--- src/components/auth/Signout.js
+++ src/components/auth/Signout.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
 class Signout extends Component {
 
@@ -10,4 +12,4 @@ class Signout extends Component {
 
 }
 
-export default Signout;
+export default connect(null, actions)(Signout);
